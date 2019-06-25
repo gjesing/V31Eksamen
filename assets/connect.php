59297cca830438443d12c0f0d01bfb7f9e093b9a
Maren Gjesing Laursen <mare0231@edu.campusvejle.dk>
2019-06-25 11:13:34 +0200

    connect.php created in assets

---
+++ assets/connect.php
@@ -0,0 +1,13 @@
+<?php
+$host = "localhost";
+$dbname = "V31Eksamen";
+$user = "root";
+$pass = "";
+
+try {
+    $dbh = new PDO("mysql:host=$host;dbname=$dbname; charset=utf8", $user, $pass);
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $ex) {
+    echo "Fejl: " . $ex;
+}
+?>
